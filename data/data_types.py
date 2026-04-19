01456ea9fdc06ea91b520ab5e5e6a6b9dd7b9731
Darshmallow <alicew061215@gmail.com>
2026-04-19 15:13:24 -0400

    data types

---
+++ data/data_types.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+
+# Epic Geometry Language
+@dataclass
+class Point:
+    name: str
+    x: float
+    y: float
+
+@dataclass
+class Line:
+    name: str
+    p1: Point
+    p2: Point
+
+@dataclass
+class Circle:
+    name: str
+    center: Point
+    radius: float
+    
+# Epic Constraint Language
+@dataclass
+class Radius:
+    circle_name: str
+    rad: float
+
+@dataclass
+class Length:
+    line_name: str
+    dist: float
+
+@dataclass
+class Intersect:
+    object_1_name: str
+    object_2_name: str
+    
+@dataclass
+class Tangent:
+    circle_name: str
+    line_name: str
+
+@dataclass
+class Angle:
+    line_1_name: str
+    line_2_name: str
+    acute_angle: float
+    
+@dataclass
+class Parallel:
+    line_1_name: str
+    line_2_name: str
+
